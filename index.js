b638829edc11c92bfe3f76942ebb4457c396e4c5
Vignesh <vigneshcse2k2k@gmail.com>
2021-06-21 17:45:24 +0530

    adding dir

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const cors = require('cors');
 const path = require('path')
 
 const port = process.env.PORT || 3000
-const app = express()
 
-app.use(express.static(__dirname + '/shopping-app/dist/shopping-app'))
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '/shopping-app/dist/shopping-app')
+
+app.use(express.static(publicDirectoryPath))
 app.use(express.json())
 app.use('/api',itemRouter)
 app.use('/api',orderRouter)
