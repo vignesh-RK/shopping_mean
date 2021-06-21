9fa2bbbf517b9534b65171ebdfab895a56eed092
Vignesh <vigneshcse2k2k@gmail.com>
2021-06-21 16:57:55 +0530

    adding hero

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path')
 const port = process.env.PORT || 3000
 const app = express()
 
-app.use(express.static(__dirname + './shopping-app/dist/shopping-app/'))
+app.use(express.static(__dirname + '/shopping-app/dist/shopping-app'))
 app.use(express.json())
 app.use('/api',itemRouter)
 app.use('/api',orderRouter)
